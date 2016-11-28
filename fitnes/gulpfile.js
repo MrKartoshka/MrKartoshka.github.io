22d640e45a39afb3862f581f9021eab1e8faa13b
DudnikMaksym <Dudnikmaksim@gmail.com>
2016-11-28 15:27:30 +0200

    V 1.0.0.

--- fitnes/gulpfile.js
+++ fitnes/gulpfile.js
@@ -4,16 +4,18 @@ var gulp         = require('gulp'),
     autoprefixer = require('autoprefixer'),
     browser      = require('browser-sync'),
     sourcemaps   = require('gulp-sourcemaps');
+    iconfont = require("gulp-iconfont"),
+    consolidate = require("gulp-consolidate");
 
 
 
 gulp.task('sass', function () {
-     return gulp.src('scss/**/*.scss')
+     return gulp.src('./src/scss/**/*.scss')
        .pipe(sourcemaps.init())
-       .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+       .pipe(sass().on('error', sass.logError))
        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
        .pipe(sourcemaps.write('.'))
-       .pipe(gulp.dest('css'))
+       .pipe(gulp.dest('build/css'))
        .pipe(browser.stream({match: '**/*.css'}));
 
 });
@@ -28,6 +30,29 @@ gulp.task('serve', ['sass'], function(){
 });
 
 gulp.task('default', ['serve'], function() {    
- gulp.watch(['scss/**/*.scss'], ['sass']);
+ gulp.watch(['./src/scss/**/*.scss'], ['sass']);
  gulp.watch('./**/*.html').on('change', browser.reload);
+});
+
+//Starts a Icon Build instance
+gulp.task("build:icons", function() {
+   return gulp.src(["src/icons/*.svg"]) //path to svg icons
+     .pipe(iconfont({
+       fontName: "fitnes-icons",
+       formats: ["ttf", "eot", "woff", "svg"],
+       centerHorizontally: true,
+       fixedWidth: true,
+       normalize: true
+     }))
+     .on("glyphs", (glyphs) => {
+
+       gulp.src("src/icons/util/*.scss") // Template for scss files
+           .pipe(consolidate("lodash", {
+               glyphs: glyphs,
+               fontName: "fitnes-icons",
+               fontPath: "../fonts/"
+           }))
+           .pipe(gulp.dest("src/scss/icons/")); // generated scss files with classes
+     })
+     .pipe(gulp.dest("build/fonts/")); // icon font destination
 });
